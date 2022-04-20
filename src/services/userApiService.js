e3977bd169e6e218ae42b057c273ae70bdd0033e
alexmorales33 <alexmoraleess4@gmail.com>
2022-04-20 01:14:54 -0300

    Perfil de Usuario 36 - Logica de autenticacion 44

--- src/services/userApiService.js
+++ src/services/userApiService.js
@@ -1,13 +1,14 @@
 import {
     deleteProfile, getProfile
   } from './apiService'
-
+  
+  const usersGetUrl = process.env.REACT_APP_API_USERS_GET
   const usersDeleteUrl = process.env.REACT_APP_API_USERS_DELETE
 
   export const getUsers = (id) => {
-    return getPrivateHandler(usersGetUrl, id)
+    return getProfile(usersGetUrl, id)
   }
 
   export const deleteUsers = (id) => {
-    return deletePrivateHandler(usersDeleteUrl, id)
+    return deleteProfile(usersDeleteUrl, id)
   }
