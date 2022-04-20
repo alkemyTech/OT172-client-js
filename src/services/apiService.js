dcde32bd9406dc026e5b5b0089278bbaa8655686
alexmorales33 <alexmoraleess4@gmail.com>
2022-04-19 23:18:29 -0300

    Perfil de Usuario 36 - Logica de autenticacion 44
    Get and delete authenticated user

---
+++ src/services/apiService.js
@@ -0,0 +1,29 @@
+import axios from 'axios'
+
+const token = localStorage.getItem('token')
+
+const apiService = axios.create({
+  baseURL: process.env.REACT_APP_BASE_URL,
+})
+
+if (token) {
+  apiService.defaults.headers.common['Authorization'] = `Bearer ${token}`
+}
+
+export const getProfile = (apiEndpoint, id) => {
+  const apiServiceUrl = id ? `${apiEndpoint}/${id}` : `${apiEndpoint}`
+  return apiService.get(apiServiceUrl)
+}
+
+export const deleteProfile = (apiEndpoint, id) => {
+  if (!id) return 'error'
+  const apiServiceUrl = `${apiEndpoint}/${id}`
+  return apiService.delete(apiServiceUrl).catch((err) => {
+    console.log(err)
+    const error = {
+      error: true,
+      message: err.message,
+    }
+    return error
+  })
+}
