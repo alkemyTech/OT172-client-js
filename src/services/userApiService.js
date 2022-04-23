3fe49465941347a7c13e73ecb9efd547f7ed9a9c
alexmorales33 <97699220+alexmorales33@users.noreply.github.com>
2022-04-23 09:40:33 -0300

    Perfil de Usuario 36 - Logica de autenticacion 44  (#2)
    * Perfil de Usuario 36 - Logica de autenticacion 44
    
    Get and delete authenticated user
    
    * Perfil de Usuario 36 - Logica de autenticacion 44
    
    Get and delete authenticated user
    
    * Perfil de Usuario 36 - Logica de autenticacion 44
    
    * Perfil de Usuario 36 - Logica de autenticacion 44
    
    * Syntaxis Error

---
+++ src/services/userApiService.js
@@ -0,0 +1,14 @@
+import {
+    deleteService, getService
+  } from './apiService'
+  
+  const usersGetUrl = process.env.REACT_APP_API_USERS_GET
+  const usersDeleteUrl = process.env.REACT_APP_API_USERS_DELETE
+
+  export const getUsers = (id) => {
+    return getService(usersGetUrl, id)
+  }
+
+  export const deleteUsers = (id) => {
+    return deleteService(usersDeleteUrl, id)
+  }
