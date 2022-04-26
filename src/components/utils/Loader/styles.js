5262b132a8a2360d30c43e002834d8667f09d6d8
Fernando Masino <49108644+fom78@users.noreply.github.com>
2022-04-26 18:04:41 -0300

    add Loader a simple Spinner (#8)

---
+++ src/components/utils/Loader/styles.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    display: flex;
+    height:200px;
+    align-items: center;
+    justify-content: center;
+    .spinner,.spinner:after {
+      display: block;
+      width: ${props => props.width || '32px' };
+      height: ${props => props.height || '32px' };
+      border-radius: 50%;
+    }
+    --line: ${props => props.line || '5px' };
+    --color: ${props => props.color ? `rgb(${props.color.r},${props.color.g},${props.color.b})` : 'rgb(166,139,202)' };
+    --color-left: ${props => props.color ? `rgb(${props.color.r},${props.color.g},${props.color.b}, .2)` : 'rgb(166,139,202, .2)' };
+    .spinner {
+      position: absolute;
+      background-color: transparent;
+      border-top: var(--line) solid var(--color);
+      border-right: var(--line) solid var(--color);
+      border-bottom: var(--line) solid var(--color);
+      border-left: var(--line) solid var(--color-left);
+      animation-iteration-count: infinite;
+      animation-timing-function: linear;
+      animation-duration: .8s;
+      animation-name: spinner-loading;
+    }
+    @keyframes spinner-loading {
+        0% {
+            transform: rotate(0deg)
+        } to {
+        transform: rotate(1turn)
+        }
+    }
+`
