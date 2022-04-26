1b3ac94f8eef0ffd0c90bdc4a1af3954624014fc
pasini_mariano <pasinimariano@gmail.com>
2022-04-26 18:16:10 -0300

    The logic of the submit button was added, when pressed it will create or not user in the data base

---
+++ src/services/apiAuth.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+import { ENDPOINT_REGISTER } from './settings'
+
+export const postUserService = async values => {
+  const response = axios
+    .post(ENDPOINT_REGISTER, values)
+    .then(() => true)
+    .catch(error => {
+      if (
+        error.response.data &&
+        error.response.data.msg === `Email ${values.email} already in use`
+      )
+        return 'El email ingresado ya existe'
+      else if (error.response.data && error.response.data.error)
+        return 'Error interno del servidor, por favor contacte con el servicio técnico'
+      else return error.response.data.msg
+    })
+
+  return response
+}
