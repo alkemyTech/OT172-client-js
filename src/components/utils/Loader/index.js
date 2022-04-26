ac9de5da9bc1622db9a2008edda22a0b5cb242f1
fom78 <fom78a@gmail.com>
2022-04-26 16:55:40 -0300

    add Loader a simple Spinner

---
+++ src/components/utils/Loader/index.js
@@ -0,0 +1,7 @@
+
+import React from 'react'
+import { Container } from './styles'
+
+export default function Loader ({size = '48px', line = '5px', color}) {
+  return <Container width={size} height={size} line={line} color={color}><span className='spinner' /></Container>
+}
