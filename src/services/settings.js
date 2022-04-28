f3904f7b2269b55219278a9782407964b9ae9695
nhoga <n9746ab@gmail.com>
2022-04-28 14:28:51 -0300

    resolving

--- src/services/settings.js
+++ src/services/settings.js
@@ -1,6 +1,6 @@
 const BASE_API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3000/'
 
-export const ENDPOINT_REGISTER = `${BASE_API_URL}auth/register`
+export const ENDPOINT_REGISTER = `${BASE_API_URL}/auth/register`
 
 export const ENDPOINT_LOGIN = `${BASE_API_URL}auth/login`
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
 import categories from './slices/categories'
-import users from './slices/users'
 
 export default configureStore({
   reducer: {
-    categories,
-    users
+    categories
   }
 })
