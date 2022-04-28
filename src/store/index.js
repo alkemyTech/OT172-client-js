83620e267a99993823c28827707df0ead20327a2
nhoga <n9746ab@gmail.com>
2022-04-28 02:01:20 -0300

    setup Redux, reducer and action get categories and display at home to test

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from 'features/counter/counterSlice'
+import categories from './slices/categories'
 
 export default configureStore({
   reducer: {
-    counter: counterReducer
+    categories
   }
 })
