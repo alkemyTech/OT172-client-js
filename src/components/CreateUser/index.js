525defc37c7019f2b2001a6a196af8f885fc3f88
pasini_mariano <pasinimariano@gmail.com>
2022-04-28 01:02:13 -0300

    The logic of the submit button was added, when pressed it will create or not user in the data base (#9)
    * Form for user creation, formik logic and schema creation with yup (reusable components)
    
    * The logic of the submit button was added, when pressed it will create or not user in the data base
    
    * Update gitignore
    
    * Update package.json

--- src/components/CreateUser/index.js
+++ src/components/CreateUser/index.js
@@ -1,21 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import { FormikForm } from '../Forms'
 import { FormField } from '../Forms/formField'
 import { createUserSchema } from '../Forms/schemas'
+import { postUserService } from '../../services/apiAuth'
 import { FormContainer } from './styles'
 
 const FormFields = response => {
   return (
     <>
       <FormField
-        name='firstname'
+        name='firstName'
         type='text'
         placeholder='Nombre'
         FormContainer={FormContainer}
       />
       <FormField
-        name='lastname'
+        name='lastName'
         type='text'
         placeholder='Apellido'
         FormContainer={FormContainer}
@@ -39,17 +40,35 @@ const FormFields = response => {
 }
 
 export const CreateUserForm = () => {
-  const [values, setValues] = useState({
-    firstname: '',
-    lastname: '',
+  const values = {
+    firstName: '',
+    lastName: '',
     email: '',
     password: ''
-  })
-  const [response, setResponse] = useState()
-
-  const handleSubmit = values => {
-    console.log('Submitting', values)
   }
+  const [response, setResponse] = useState({ ok: false, msg: '' })
+
+  const handleSubmit = async (values, { resetForm }) => {
+    const serviceResponse = await postUserService(values)
+
+    if (serviceResponse === true) {
+      setResponse(prevState => ({
+        ...prevState,
+        ok: true,
+        msg: `El usuario ${values.email} fue creado exitosamente`
+      }))
+      resetForm({ values: '' })
+    } else {
+      setResponse(prevState => ({
+        ...prevState,
+        msg: serviceResponse
+      }))
+    }
+  }
+
+  useEffect(() => {
+    setTimeout(() => setResponse({ ok: false, msg: '' }), 30000)
+  }, [response])
 
   return (
     <FormikForm
@@ -58,7 +77,7 @@ export const CreateUserForm = () => {
       values={values}
       schema={createUserSchema}
       onSubmit={handleSubmit}
-      FormFields={() => FormFields(response)}
+      FormFields={() => FormFields(response.msg)}
     />
   )
 }
