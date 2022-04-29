99b7a5c61cb4d4ebfa622bcee83f759860543ae3
Fernando <fom78a@gmail.com>
2022-04-29 00:29:45 -0300

    add organization slice

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import categories from './slices/categories'
+import organization from './slices/organization'
 import users from './slices/users'
 
 
 export default configureStore({
   reducer: {
     categories,
+    organization,
     users
   }
 })
