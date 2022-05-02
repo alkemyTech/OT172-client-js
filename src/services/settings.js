e0c59c2740b2a5f22d4816b3e23d8df9066659eb
ManuelLatorre98 <manuel.latorre@est.fi.uncoma.edu.ar>
2022-05-02 09:10:45 -0300

    fixed directions with // instead /

--- src/services/settings.js
+++ src/services/settings.js
@@ -1,13 +1,13 @@
 const BASE_API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3000/'
 
-export const ENDPOINT_REGISTER = `${BASE_API_URL}/auth/register`
+export const ENDPOINT_REGISTER = `${BASE_API_URL}auth/register`
 
 export const ENDPOINT_LOGIN = `${BASE_API_URL}auth/login`
 
 export const ENDPOINT_GETLOGGED = `${BASE_API_URL}auth/me`
 
-export const ENDPOINT_PUBLIC = `${BASE_API_URL}/organization/1`
+export const ENDPOINT_PUBLIC = `${BASE_API_URL}organization/1`
 
-export const ENDPOINT_CATEGORIES = `${BASE_API_URL}/categories`
+export const ENDPOINT_CATEGORIES = `${BASE_API_URL}categories`
 
-export const ENDPOINT_NEWS = `${BASE_API_URL}/news`
+export const ENDPOINT_NEWS = `${BASE_API_URL}news`
