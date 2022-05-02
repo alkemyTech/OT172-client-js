92c21acf270f2e1f531692a6c5a26838197e84e8
Fernando <fom78a@gmail.com>
2022-05-02 03:24:26 -0300

    users slice

--- src/store/slices/users/index.js
+++ src/store/slices/users/index.js
@@ -1,9 +1,11 @@
 import { createSlice } from '@reduxjs/toolkit'
-import axios from 'axios'
+import { getService } from 'services/apiService'
+import { ENDPOINT_USERS } from "services/settings"
+
 
 export const userSlice = createSlice({
     name: 'users',
-    initialState: [
+    initialState: {list: [
         {
           id: 0, 
           firstName: "",
@@ -13,7 +15,7 @@ export const userSlice = createSlice({
           roleId: null,
           image: ""
         }
-    ],
+    ]},
     reducers: {
     addUser: (state,action) => {
        state.push(action.payload)
@@ -21,10 +23,22 @@ export const userSlice = createSlice({
     },
     getUsers: (state,action) => {
         return state
+    },
+    setUsers: (state,action) => {
+        state.list = action.payload
     }
     }
 })
 
-export const { addUser, getUsers } = userSlice.actions
+export const { addUser, getUsers, setUsers } = userSlice.actions
 
 export default userSlice.reducer
+
+export const fetchAllUsers = () => async (dispatch) => {
+    try {
+        const response = await getService(ENDPOINT_USERS)
+        dispatch(setUsers(response.data))
+    } catch (error) {
+        console.log(error.message)
+    }
+}
