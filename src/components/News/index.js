1d0f78a75085e7e6fffd57071656997d59d5a8db
Fernando Masino <49108644+fom78@users.noreply.github.com>
2022-05-02 16:06:23 -0300

    Fix state (#25)
    * add fetcher function
    
    * slice with createAsyncThunk

--- src/components/News/index.js
+++ src/components/News/index.js
@@ -1,24 +1,29 @@
 import React, { useEffect } from "react"
 import { useDispatch } from "react-redux";
 import { useSelector } from 'react-redux'
-import { fetchAllNews } from "store/slices/news";
+import { fetchAllNews, reset } from "store/slices/news";
 import CardNew from "./CardNew";
 import { Container } from "./styles";
 
 export default function News({qty = 'all', title = ''}) {
-  const { list: news } = useSelector(state => state.news)
+  const { news } = useSelector(state => state.news)
+  const { user } = useSelector(state => state.auth)
   const dispatch = useDispatch()
   
   useEffect(() => {
     dispatch(fetchAllNews())
-  }, [dispatch])
+    if (!user) {
+    dispatch(reset())
+  }
+
+  }, [dispatch, user])
 
   const totalShowNews = qty === 'all' ? news.length : qty
 
   return (
     <Container>
       <h1>{title}</h1>
-      {news.length > 0 
+      {news?.length > 0 
         ? news.slice(0,totalShowNews).map( (n, i) => <CardNew key={i} element={n} /> )
         : <p>No hay noticias</p>
       }
--- src/store/slices/news/index.js
+++ src/store/slices/news/index.js
@@ -1,28 +1,57 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 import { getService } from 'services/apiService'
 import { ENDPOINT_NEWS } from "services/settings"
 
+const initialState = {
+    news: [],
+    isError: false,
+    isSuccess: false,
+    isLoading: false,
+    message: ''
+}
+
 export const newSlice = createSlice({
     name: 'news',
-    initialState: {
-        list: []
-    },
+    initialState,
     reducers: {
-    setNewsList: (state,action) => {
-        state.list = action.payload
-    }
-    }
-})
+        reset: (state) => {
+            state.isLoading = false
+            state.isSuccess = false
+            state.isError = false
+            state.message = ''
+            state.news = []
+          },
+    },
+    extraReducers: (builder) => {
+      builder
+        .addCase(fetchAllNews.pending, (state) => {
+          state.isLoading = true
+        })
+        .addCase(fetchAllNews.fulfilled, (state, action) => {
+          state.isLoading = false
+          state.isSuccess = true
+          state.news = action.payload
+        })
+        .addCase(fetchAllNews.rejected, (state, action) => {
+          state.isLoading = false
+          state.isError = true
+          state.message = action.payload
+          state.news = []
+        })
+    },
+  })
 
-export const { setNewsList } = newSlice.actions
+export const { reset } = newSlice.actions
 
 export default newSlice.reducer
 
-export const fetchAllNews = () => async (dispatch) => {
+export const fetchAllNews = createAsyncThunk('news',async (thunkAPI) => {
     try {
-        const response = await getService(ENDPOINT_NEWS)
-        dispatch(setNewsList(response.data.newsList))
+        const data = await getService(ENDPOINT_NEWS)
+        console.log('la data', data);
+        return data.data.newsList
     } catch (error) {
-        console.log(error.message)
+        const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+        return thunkAPI.rejectWithValue(message)
     }
-}
+}) 
