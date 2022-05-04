b9746799746efb281ccd99cf23f2e67bd9b837ce
ManuelLatorre98 <manuel.latorre@est.fi.uncoma.edu.ar>
2022-05-04 19:07:57 -0300

    Registration process working with form

--- src/components/CreateUser/index.js
+++ src/components/CreateUser/index.js
@@ -1,12 +1,9 @@
-import React, { useState, useEffect } from 'react'
-
+import React, { useEffect } from 'react'
 import { FormikForm } from '../Forms'
 import { FormField } from '../Forms/formField'
 import { createUserSchema } from '../Forms/schemas'
-import { postUserService } from '../../services/apiAuth'
 import { FormContainer } from './styles'
 import { useDispatch, useSelector } from 'react-redux'
-import { addUser } from '../../store/slices/users'
 import { register, reset } from '../../store/slices/auth/index'
 import { alertToast } from 'services/alerts'
 import { useNavigate } from 'react-router-dom'
@@ -46,62 +43,35 @@ const FormFields = response => {
 
 export const CreateUserForm = () => {
 const dispatch = useDispatch()
-const { users } = useSelector(state => state.categories)
-const [response, setResponse] = useState({ ok: false, msg: '' })
-
   const values = {
     firstName: '',
     lastName: '',
     email: '',
     password: ''
   }
-
+    
   const navigate = useNavigate()
   
   const {user, isLoading, isError, isSuccess, message} = useSelector((state) => state.auth) 
   
   useEffect(() => {
-  if(isError){
-    alertToast('error',message)
-  }
-  if(isSuccess || user){
-    alertToast('success','Registro con exito!')
-    navigate('/home')
-  }
-  dispatch(reset())
+    if(isError){
+      alertToast('error',message)
+    }
+    if(isSuccess || user){
+      alertToast('success','Registro con exito!')
+      navigate('/home')
+    }
+    dispatch(reset())
   }, [user, isError, isSuccess, message, navigate, dispatch])
-  
 
-
-  const handleSubmit = async (values, { resetForm }) => {
-    console.log(values)
+  const handleSubmit = (values) => {
     dispatch(register(values))
-    /*const serviceResponse = await postUserService(values)
-
-    if (serviceResponse === true) {
-      addUser(prevState => ({
-        ...prevState,
-              }))
-              setResponse(prevState => ({
-                ...prevState,
-                ok: true,
-                msg: `El usuario ${values.email} fue creado exitosamente`
-              }))
-      resetForm({ values: '' })
-    } else {
-      setResponse(prevState => ({
-        ...prevState,
-        msg: serviceResponse
-      }))
-      addUser(prevState => ({
-        ...prevState,
-      }))
-    }*/
   }
 
-  /*useEffect(() => {
-    dispatch(addUser())
-  }, [dispatch])*/
+  /*if(isLoading) {
+      //Loading screen, check preserve the state of the fields for not write all again after re-render
+  }*/
 
   return (
     <FormikForm
@@ -110,7 +80,7 @@ const [response, setResponse] = useState({ ok: false, msg: '' })
       values={values}
       schema={createUserSchema}
       onSubmit={handleSubmit}
-      FormFields={() => FormFields(response.msg)}
+      FormFields={() => FormFields()}
     />
   )
 }
