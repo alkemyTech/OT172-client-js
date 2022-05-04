eb7790965062d00ac1382e6cb14ad3c9cdc57a40
Fernando <fom78a@gmail.com>
2022-05-04 20:10:23 -0300

    refactor Users

--- src/components/BackOffice/Users/index.js
+++ src/components/BackOffice/Users/index.js
@@ -1,11 +1,17 @@
 import React from 'react'
-import UserCard from '../User'
+import UserCard from '../UserCard'
+import { Container, Head } from './styles'
 
 export default function UsersList({ users }) {
   return (
-    <>
+    <Container>
       <h1>Usuarios registrados en el sistema</h1>
+      <Head>
+        <div className='email' >Email</div>
+        <div className='names'>Names</div>
+        <div className='actions'>{users.length}</div>
+      </Head>
       {users?.map((u, index) => <UserCard key={index} user={u} />)}
-    </>
+    </Container>
   )
 }
