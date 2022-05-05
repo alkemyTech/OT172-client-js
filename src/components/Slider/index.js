494074d66c251e53c6ff8cf41c7c7116ea0d88f9
Fernando <fom78a@gmail.com>
2022-05-05 17:24:18 -0300

    add Slider component

---
+++ src/components/Slider/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {Slideshow, Slide, TextoSlide} from './Slideshow'
+import styled from 'styled-components';
+import { images } from './images';
+
+const Slider = ({title=''}) => {
+	return (
+		<Container>
+			<Titulo>{title}</Titulo>
+			<Slideshow controles={true} autoplay={true} velocidad="1000" intervalo="2000">
+				{images.map(img => <Slide>
+          <a href={img.link}>
+						<img src={img.img} alt=""/>
+					</a>
+					<TextoSlide>
+						<p>{img.text}</p>
+					</TextoSlide>
+        </Slide>
+        )}
+			</Slideshow>
+		</Container>
+	);
+}
+
+const Container = styled.section`
+	max-width: 1000px;
+	margin: auto;
+	overflow: hidden;
+`
+const Titulo = styled.p`
+	font-size: 18px;
+	font-weight: 700;
+	text-transform: uppercase;
+	margin-bottom: 10px;
+`;
+ 
+export default Slider;
