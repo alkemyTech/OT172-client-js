eb067294ccca9b01056ed2d0fca2f4efaa0d7243
Fernando Masino <49108644+fom78@users.noreply.github.com>
2022-05-05 10:40:04 -0300

    News 47 (#28)
    * add NotFound component
    
    * add page New for see datail about one new in route news/id
    
    * add route news/id
    
    * refactor News component

--- src/components/News/CardNew/index.js
+++ src/components/News/CardNew/index.js
@@ -1,20 +1,16 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 import { alertToast, confirm } from 'services/alerts';
 import { Card } from './styles';
 
-export default function CardNew( {element} ) {
+export default function CardNew({ element }) {
   const [borrar, setBorrar] = useState(false)
+
   return (
-    <Card>
+    <Card    >
       {element.name}
-      <img src={element.image} alt={element.name} height='100px' width='auto'/>
+      <img src={element.image} alt={element.name} height='100px' width='auto' />
 
-      {/* Solo para probar el servicio de alertas, cuando se implemente borrar. */}
-      <button onClick={() => alertToast('error', 'Mensaje de error o lo que sea')}>alerta</button>
-      <button onClick={() => confirm(setBorrar, !borrar, 'Desea modificar el estado')}>{borrar ? 'DesBorrar' : 'Borrar'}</button>
-      {/* Solo para probar el servicio de alertas, cuando se implemente borrar. */}
-
-    
     </Card>
   )
 }
--- src/components/News/index.js
+++ src/components/News/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react"
 import { useDispatch } from "react-redux";
 import { useSelector } from 'react-redux'
+import { useNavigate } from "react-router-dom";
 import { fetchAllNews, reset } from "store/slices/news";
 import CardNew from "./CardNew";
 import { Container } from "./styles";
@@ -18,13 +19,25 @@ export default function News({qty = 'all', title = ''}) {
 
   }, [dispatch, user])
 
+  const navigate = useNavigate()
+
+  const handleShowNew = (element) => {
+    navigate(`/news/${element.id}`)
+  }
+
   const totalShowNews = qty === 'all' ? news.length : qty
 
   return (
     <Container>
       <h1>{title}</h1>
       {news?.length > 0 
-        ? news.slice(0,totalShowNews).map( (n, i) => <CardNew key={i} element={n} /> )
+        ? news.slice(0,totalShowNews).map( (n, i) => 
+          <div key={i} onClick={() => handleShowNew(n)}>
+          <CardNew 
+            key={i} 
+            element={n} 
+             
+          /> </div>)
         : <p>No hay noticias</p>
       }
     </Container>
