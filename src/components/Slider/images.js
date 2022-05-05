21d857fbfc6a002995f8352ae91625485b1e2a02
Fernando <fom78a@gmail.com>
2022-05-05 17:23:19 -0300

    add images for slide

---
+++ src/components/Slider/images.js
@@ -0,0 +1,27 @@
+import img1 from './img/blog-img-01.jpg'
+import img2 from './img/blog-img-02.jpg'
+import img3 from './img/blog-img-03.jpg'
+import img4 from './img/blog-img-04.jpg'
+
+export const images = [
+  {
+    link:'http://localhost:3005/home',
+    img: img1,
+    text: 'Texto descriptivo para la imagen'
+  },
+  {
+    link:'http://localhost:3005/home',
+    img: img2,
+    text: 'Texto descriptivo para la imagen'
+  },
+  {
+    link:'http://localhost:3005/home',
+    img: img3,
+    text: 'Texto descriptivo para la imagen'
+  },
+  {
+    link:'http://localhost:3005/home',
+    img: img4,
+    text: 'Texto descriptivo para la imagen'
+  }
+]
