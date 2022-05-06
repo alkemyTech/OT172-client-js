0c2a083c48b81036a232fd594bcfb5b32d378e65
fom78 <fom78a@gmail.com>
2022-05-05 23:42:18 -0300

    new with detail option

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -4,38 +4,44 @@ import NotFound from 'components/utils/NotFound'
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link, useParams } from 'react-router-dom'
-import { fetchAllNews, reset } from 'store/slices/news'
+import { getService } from 'services/apiService'
+import { ENDPOINT_NEWS } from 'services/settings'
+import { fetchAllCategories } from 'store/slices/categories'
 import { Container } from "./styles"
 
 function New() {
   const params = useParams()
-  const  [newtoShow, setNewToShow] = useState({})
-  const { user } = useSelector(state => state.auth)
-  const { news, isLoading } = useSelector((state) => state.news)
+  const  [isLoading, setIsLoading] = useState(false)
+  const  [data, setData] = useState({})
+  
+  const {categories} = useSelector(state => state.categories)
   const dispatch = useDispatch()
-
+  
+  useEffect(() => {
+    dispatch(fetchAllCategories())
+  }, [dispatch])
 
   useEffect(() => {
-    dispatch(fetchAllNews())
-
-    if (!user) {
-      dispatch(reset())
-    }
-  }, [dispatch, user])
-
-  useEffect(() => {
-    const newFounded = news.filter(e => e.id === parseInt(params.id))
-    setNewToShow(newFounded[0])
-  }, [params.id, news])
+    (async () => {
+      setIsLoading(true)
+      if (params.id) {
+        const response = await getService(ENDPOINT_NEWS, params.id)
+        setData(response.data)
+        console.log(categories);
+        setIsLoading(false)
+      }
+    })();
+  }, [params.id, getService]);
 
   if (isLoading) return <Loader />
 
   return (
     <Container>
-      <Link to={`/news`}>Volver</Link>
-      {newtoShow
+      <Link to={`/news`}>Volver a Novedades</Link>
+      {data
        ? <CardNew 
-            element={newtoShow} 
+            data={data}
+            detail
           />
        : <NotFound resource='New' msg='Verifique la ruta que intenta acceder'/>
        }
