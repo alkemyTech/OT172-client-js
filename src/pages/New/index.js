2eab4500b02d0e9070b96e1b3b5057798700e6a9
Fernando <fom78a@gmail.com>
2022-05-06 14:46:29 -0300

    add icon for link to return

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -2,24 +2,17 @@ import CardNew from 'components/News/CardNew'
 import Loader from 'components/utils/Loader'
 import NotFound from 'components/utils/NotFound'
 import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
 import { Link, useParams } from 'react-router-dom'
 import { getService } from 'services/apiService'
 import { ENDPOINT_NEWS } from 'services/settings'
-import { fetchAllCategories } from 'store/slices/categories'
 import { Container } from "./styles"
+import { TiArrowBack } from "react-icons/ti";
 
 function New() {
   const params = useParams()
   const  [isLoading, setIsLoading] = useState(false)
   const  [data, setData] = useState({})
   
-  const {categories} = useSelector(state => state.categories)
-  const dispatch = useDispatch()
-  
-  useEffect(() => {
-    dispatch(fetchAllCategories())
-  }, [dispatch])
 
   useEffect(() => {
     (async () => {
@@ -27,7 +20,6 @@ function New() {
       if (params.id) {
         const response = await getService(ENDPOINT_NEWS, params.id)
         setData(response.data)
-        console.log(categories);
         setIsLoading(false)
       }
     })();
@@ -37,7 +29,7 @@ function New() {
 
   return (
     <Container>
-      <Link to={`/news`}>Volver a Novedades</Link>
+      <Link to={`/news`}><TiArrowBack /> Volver a Novedades</Link>
       {data
        ? <CardNew 
             data={data}
