787dac727ca250cc83f583cec7f4b5d72fe25720
fom78 <fom78a@gmail.com>
2022-05-12 22:49:32 -0300

    routes privates, protectes and link active

---
+++ src/routes/404.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export default function Page404() {
+  return (
+    <Container>
+      <img src="/images/404.png" alt="No encontrado" />
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  text-align: center;
+  width: 100vw;
+  padding: 3rem;
+`
