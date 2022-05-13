6ec99cd5afdbcc4d4c535540f87c8540fd969a2f
Nicolás Alexis Borzone <n9746ab@gmail.com>
2022-05-13 20:56:41 -0300

    sendgrid (#41)
    Co-authored-by: Nicolás A Borzone <nicolasborzone@any2cloud.com>
    Co-authored-by: Fernando Masino <49108644+fom78@users.noreply.github.com>

--- src/components/CreateUser/index.js
+++ src/components/CreateUser/index.js
@@ -7,6 +7,10 @@ import { useDispatch, useSelector } from 'react-redux'
 import { register, reset } from '../../store/slices/auth/index'
 import { alertToast } from 'services/alerts'
 import { useNavigate } from 'react-router-dom'
+import { Form } from 'formik'
+import { getDefaultNormalizer } from '@testing-library/react'
+import { ENDPOINT_MAIL } from 'services/settings'
+import axios from 'axios'
 
 const FormFields = response => {
   return (
@@ -52,9 +56,33 @@ export const CreateUserForm = () => {
     password: ''
   }
 
+  const sgMail = require('@sendgrid/mail')
+  sgMail.setApiKey('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')
+
+  const onComplete = async (values) => {
+    const message = {
+      to: values.email,
+      from: 'n9746ab@gmail',
+      subject: 'Registro',
+      text:"Registro completado",
+      html: '<><p>Registro completado, felicitaciones! </p></>'
+    }
+    await axios.post(ENDPOINT_MAIL, message)
+    .then(() => {
+      console.log('Email send')
+    }).catch((err) => {
+      console.error(err.message)
+    })
+  }
+
+
+
+
   //Register req
   const handleSubmit = values => {
+    console.log(values)
     dispatch(register(values))
+    onComplete(values)
   }
 
   //Effects/notifications 
