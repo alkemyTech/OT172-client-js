4f172fbc838a6f3026f0cba549bcc15bac1aa056
fom78 <fom78a@gmail.com>
2022-05-12 22:06:45 -0300

    Navbar with active link

--- src/routes/PrivatedRoute.js
+++ src/routes/PrivatedRoute.js
@@ -6,7 +6,6 @@ import { useSelector } from 'react-redux'
 
 export function PrivatedRoute ({ children }) {
   const { user, isLoading } = useSelector(state => state.auth)
-console.log(user);
   if (isLoading) return <Loader />
 
   if (user.roleId !== 1) return <Navigate to='/home' />
