4bbfdea13d85a2b17512e9daece83d3b68eacfee
Fernando <fom78a@gmail.com>
2022-05-15 09:14:17 -0300

    style categories backoffice

--- src/common/styles.js
+++ src/common/styles.js
@@ -1,5 +1,24 @@
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
+import { mediaQueries } from 'config/responsive'
+const { screenLittle, screenMiddle } = mediaQueries
+
+export const ContainerListBackOffice = styled.section`
+  margin: auto ;
+  color: ${({theme}) => theme.primaryDark};
+  text-align: left ;
+  &>h1 {
+    text-align: center;
+    font-size: 1.5rem ;
+  }
+
+  ${screenLittle} {
+    width: 500px ;
+  }
+  ${screenMiddle} {
+    width: 700px ;
+  }
+`
 
 export const LinkStyled = styled(Link)`
  font-size: 1rem;
@@ -8,4 +27,14 @@ export const LinkStyled = styled(Link)`
    color: var(--blue-02);
    margin-right: .3rem ;
  }
+`
+
+export const HeadTableBackOffice = styled.div`
+  display: flex;
+  justify-content: space-between;
+  border: 2px solid #006600;
+  font-weight: 700;
+  margin: .2rem;
+  margin-top: 1rem ;
+  padding: .2rem ;
 `
