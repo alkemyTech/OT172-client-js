5af5194a8710413c55ace68b8db69b0468a6b466
fom78 <fom78a@gmail.com>
2022-05-16 16:07:15 -0300

    add middleware for store

--- src/store/index.js
+++ src/store/index.js
@@ -19,5 +19,16 @@ export default configureStore({
     organization,
     auth,
     contacts
-  }
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        // Ignore these action types
+        ignoredActions: ['create/activities/fulfilled','activities/pending','create/news/fulfilled','create/news/fulfilled','categories/pending','news/pending','categories/fulfilled'],
+        // Ignore these field paths in all actions
+        ignoredActionPaths: ['meta.arg', 'payload.timestamp'],
+        // Ignore these paths in the state
+        ignoredPaths: ['createActivities.pending','activities.activities.5.image','payload.image','news.news.3.image'],
+      },
+    }),
 })
