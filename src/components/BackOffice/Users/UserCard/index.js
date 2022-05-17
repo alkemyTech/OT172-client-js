c68a770d7f887337b30fc5a6add00b9b4f40d734
fom78 <fom78a@gmail.com>
2022-05-17 16:07:10 -0300

    edit user card for backoffice

--- src/components/BackOffice/Users/UserCard/index.js
+++ src/components/BackOffice/Users/UserCard/index.js
@@ -1,18 +1,43 @@
 import { CardBackOffice } from 'common/styles';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { FaTrash, FaEdit } from 'react-icons/fa'
+import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
-import { confirm } from 'services/alerts';
-import { deleteUser } from 'store/slices/users';
+import { useNavigate } from 'react-router-dom';
+import { alertToast, confirm } from 'services/alerts';
+import { deleteUsers } from 'store/slices/users';
 import { Row } from './styles'
 
 
 export default function UserCard({ user }) {
+  const { isError, isSuccess, message } = useSelector(state => state.users)
+
+  const [deleteUser, setDeleteUser] = useState(false)
   const dispatch = useDispatch()
 
+  const navigate = useNavigate()
+
   const handleEdit = (id) => {
-    console.log(`el user id es: ${id}`);
+    navigate(`${id}`)
   }
+
+  const handleDelete = () => {
+    confirm(() => setDeleteUser(true), 'Eliminar la Usuario ?')
+  }
+
+  useEffect(() => {
+    if (deleteUser) {
+      dispatch(deleteUsers(user.id))
+    }
+  }, [deleteUser])
+
+  useEffect(() => {
+    if (isError) alertToast('error', message)
+    if (deleteUser && isSuccess) alertToast('success', 'Usuario Eliminada correctamente')
+    setDeleteUser(false)
+  }, [isError, dispatch, deleteUser])
+
+
   return (
     <CardBackOffice>
       <Row>
@@ -24,13 +49,15 @@ export default function UserCard({ user }) {
           >
             <FaEdit />
           </button>
-          <button 
-            onClick={() => confirm(() => dispatch(deleteUser(user.id)), `Eliminar el usuario con email: ${user.email} ?`)}
+          <button
+            onClick={() => handleDelete()}
           >
             <FaTrash />
           </button>
         </div>
       </Row>
+
+
     </CardBackOffice>
   )
 }
