08245806140b34a22bb0d78fd787fb0eee03392a
ManuelLatorre98 <manuel.latorre@est.fi.uncoma.edu.ar>
2022-05-20 13:49:31 -0300

    Added dynamic page name update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import Header from 'components/Header'
 import Footer from './components/Footer'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { Toaster } from 'react-hot-toast'
 import { fetchOrganizationDataPublic } from 'store/slices/organization'
 import RoutesWeb from 'routes/routesWeb'
@@ -16,10 +16,16 @@ const toastOptions = {
 
 function App () {
   const dispatch = useDispatch()
+  const { list } = useSelector((state) => state.organization)
   useEffect(() => {
+    //document.title= list.name
     dispatch(fetchOrganizationDataPublic(1))
   }, [dispatch])
 
+  useEffect(()=>{ //This change the name of the page when reducer updates
+    document.title= list.name
+  })
+  
   return (
     <ThemeProvider theme={theme}>
       <>
