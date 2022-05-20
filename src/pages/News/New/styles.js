c5b0546afeda7b9649d4e577a41fcf1ea71c3e2d
ManuelLatorre98 <manuel.latorre@est.fi.uncoma.edu.ar>
2022-05-20 19:15:59 -0300

    New style to cards

--- src/pages/News/New/styles.js
+++ src/pages/News/New/styles.js
@@ -1,19 +1,6 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  color: black;
-  padding-top: 1rem;
-
-  a {
-    text-align: center ;
-    font-size: 1rem;
-    text-decoration: none;
-    &:hover {
-      color: #0000FF
-    }
-    svg {
-      padding-left: 2rem ;
-    }
-  }
-  
+padding-top: 1rem;
+text-align: left;  
 `
